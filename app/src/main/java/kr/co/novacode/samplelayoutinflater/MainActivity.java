d1e76461a239046cca664701bbdc35d3ab2fd3da
verfass <verfass@gmail.com>
2017-01-13 11:27:43 +0900

    두번째 commit

--- app/src/main/java/kr/co/novacode/samplelayoutinflater/MainActivity.java
+++ app/src/main/java/kr/co/novacode/samplelayoutinflater/MainActivity.java
@@ -1,7 +1,13 @@
 package kr.co.novacode.samplelayoutinflater;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button01 = (Button)findViewById(R.id.button01);
+        button01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inflateLayout();
+            }
+        });
+    }
+
+    private void inflateLayout() {
+        LinearLayout contentsLayout = (LinearLayout)findViewById(R.id.contentsLayout);
+
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.button, contentsLayout, true);
+
+        Button selectButton = (Button)findViewById(R.id.selectButton);
+        final CheckBox allDay = (CheckBox)findViewById(R.id.allDay);
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allDay.isChecked()) {
+                    allDay.setChecked(false);
+                } else {
+                    allDay.setChecked(true);
+                }
+            }
+        });
     }
 }
